784e2436912d5b5b6942d29245a8233bf55a2d75
IhebBenTaher <iheb.ben.taher1@gmail.com>
2025-05-14 22:05:40 +0100

    v2

--- src/main/java/com/example/cicd/HelloController.java
+++ src/main/java/com/example/cicd/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String welcome(){
-        return "welcome";
+        return "welcome v2";
     }
 }
